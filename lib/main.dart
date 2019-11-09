a9d21e2bf8c7e567b2e2c6b73621e991584f656d
devabacusnrf <devabacusnrf@gmail.com>
2019-11-09 09:48:05 +0500

    future delayed2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,16 +11,32 @@ class MyApp extends StatelessWidget {
     await Future.delayed(Duration(seconds: 5));
     return 'Message from future';
   }
-
   test() async{
       String x = await helloAsync();
       print(x);
   }
 
+
+  streamSub(Stream stream1) async {
+    print("Вызвали функцию только один раз. Остально асинхронный стрим");
+    await for(int i in stream1) {
+      print('Подписаны на новые значения. Получили: $i');
+    }
+  }
+
+  test1() async {
+    Duration interval = Duration(seconds: 2);
+    Stream<int> stream = Stream<int>.periodic(interval, CallBackEvery_2seconds);
+    streamSub(stream);
+  }
+
+  int CallBackEvery_2seconds(int computationCount) {
+    print('computationCountf = $computationCount');
+    return (computationCount + 1) * 2;
+  }
+
   @override
   Widget build(BuildContext context) {
-    test();
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -27,7 +45,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("Stream learn"),),
         body: Column(children: <Widget>[
-          RaisedButton(child: Text('Задержка 5 сек'), onPressed: test,)
+          RaisedButton(child: Text('Задержка 5 сек'), onPressed: test1,)
         ],)
       ),
     );
